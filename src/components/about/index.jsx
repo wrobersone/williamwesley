d8dcc92423b49c5ecd1370c7d7529db221b61bff
William Robersone <william.robersone@gmail.com>
2023-05-12 20:57:52 -0500

    noreferrer

--- src/components/about/index.jsx
+++ src/components/about/index.jsx
@@ -41,7 +41,7 @@ const index = () => {
               {/* End personal info */}
 
               <div className="col-12 mt-1">
-                <a className="button" href={'https://drive.google.com/file/d/15Y4lfP1sfMijLjswQ4khOB3gQiJrUTiZ/view?usp=share_link'} target="_blank" download>
+                <a className="button" href={'https://drive.google.com/file/d/15Y4lfP1sfMijLjswQ4khOB3gQiJrUTiZ/view?usp=share_link'} target="_blank" rel="noreferrer" download>
                   <span className="button-text">Download Resume</span>
                   <span className="button-icon fa fa-download"></span>
                 </a>
